897aa488e5121b7e1e919add779f4b74a7465df8
floogal <67554336+floogal@users.noreply.github.com>
2021-11-21 08:44:00 +0000

    initial commit

---
+++ emulator2/emu2.php
@@ -0,0 +1,337 @@
+﻿<html>
+<body>
+<style>
+body{margin:0;overflow:hidden;}
+body,table,td {font-size:14px; font-family: Arial, Helvetica, sans-serif;}
+#waflashStatus{
+	position:absolute;top:50%;left:50%;-webkit-transform:translate(-50%,-50%);-ms-transform:translate(-50%,-50%);transform:translate(-50%,-50%);
+	width:70%;color:white;margin:0px auto;padding:10px;font-size:16px;text-align:center;background-color:rgba(0,0,0,0.3);display:none;
+}
+#context-menu {
+    color: black;
+    background-color: #FAFAFA;
+    border: 1px solid gray;
+    box-shadow: 0px 5px 10px -5px black;
+    position: absolute;
+    font-size: 14px;
+    text-align: left;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+#context-menu .menu_item {
+    padding: 5px 10px;
+}
+#context-menu .active {
+    cursor: pointer;
+    color: black;
+}
+#context-menu .disabled {
+    cursor: default;
+    color: gray;
+}
+#context-menu .active:hover {
+    background-color: lightgray;
+}
+#context-menu .menu_separator hr {
+    border: none;
+    border-bottom: 1px solid lightgray;
+    margin: 2px;
+}
+/*
+#right_click_menu {
+    color: #FFAD33;
+    background-color: #37528c;
+    border-radius: 5px;
+    box-shadow: 0px 5px 15px -5px #000;
+    position: absolute;
+    font-size: 16px;
+    text-align: left;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+#right_click_menu .menu_item {
+    padding: 5px 10px;
+}
+#right_click_menu .menu_separator {
+    padding: 5px 5px;
+}
+#right_click_menu .active {
+    cursor: pointer;
+    color: #FFAD33;
+}
+#right_click_menu .disabled {
+    cursor: default;
+    color: #94672F;
+}
+#right_click_menu .active:hover {
+    background-color: #184778;
+}
+#right_click_menu hr {
+    color: #FFAD33;
+}
+#right_click_menu > :first-child {
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+}
+#right_click_menu > :last-child {
+    border-bottom-right-radius: 5px;
+    border-bottom-left-radius: 5px;
+}*/
+</style>
+
+<table id="loading" style="position:absolute;left:10px;top:20px;background: white;padding:3px;border:0px solid silver;-webkit-box-shadow: 0 0 10px #999;-moz-box-shadow: 0 0 10px #999; box-shadow: 0 0 10px #999;">
+<tr><td><img src="etc/wait.gif">
+<td>Loading library... Please wait a moment.
+</table>
+
+<div id="waflashContainer" style="width:100%;height:100%;position:relative;border:0px solid black">
+<canvas id="canvas" tabindex="1" style="width:100%;height:100%;cursor: default;outline: none;"></canvas>
+<div id="waflashStatus" style="display:none;"></div>
+</div>
+<ul id="context-menu" style="display:none"><li id="menu1" class="menu_item active" style="white-space:nowrap">Enter fullscreen (Emulator 2)</li></ul>
+
+<script>
+var gainNode;
+function proc_gain(gain){
+try{
+	if(gain && gain.gain){
+		gainNode=gain;		
+		setTimeout(function(){
+			if(parent && parent.volume1_onchange) parent.volume1_onchange();
+			/*var s=parseInt(localStorage['volume1']);
+			if(!isNaN(s)){	
+				if(s<0) s=0; if(s>100) s=100;
+				gainNode.gain.value=s/100;
+			}*/
+		},100);
+	}
+}catch(err){}
+}
+function proc_volume(value){
+	if(gainNode) gainNode.gain.value=value;
+}
+var golddate=Date;
+Date = function(){
+	var a=new golddate();
+	a.setFullYear('2020'); a.setMonth('11'); a.setDate('10');
+	return a;
+};
+Date.now=function(){
+	var a=new golddate();
+	a.setFullYear('2020'); a.setMonth('11'); a.setDate('10');
+	return a.getTime();
+}
+</script>
+<script type="module">
+import createWaflashModule from 'https://clubpenguinadvanced.github.io/waflash-demo/emulator2/waflash_latest/waflash.js?t=1';
+var WAFLASH_BASE_URL = "https://clubpenguinadvanced.github.io/waflash-demo/emulator2/waflash_latest/";
+var loaded=false;
+
+function _getid(id){
+	return document.getElementById(id);
+}
+function init(){
+    var waflash = {
+        arguments: [''],
+        preRun: [],
+        postRun: [],
+        locateFile(path, prefix) {
+            return WAFLASH_BASE_URL + path;
+        },
+        print(text) {
+            //console.log(text);
+        },
+        printErr(text) {
+            //console.error(text);
+        },
+        canvas: (function () {
+            var canvas = _getid("canvas");
+            canvas.addEventListener("webglcontextlost", function (e){
+				waflash.setStatus('WebGL context lost. Reload the Flash file, or Open the another Flash file.');
+				e.preventDefault(); 
+			}, false);
+            return canvas;
+        })(),
+        statusElement: (function(){
+			return _getid('waflashStatus');
+		})(),
+        setStatus: function(text){
+            if(!text || !loaded) return;
+			if(/(Reload the page)/.test(text)){
+				text='An error occurred. Reload the Flash file, or Open the another Flash file.';
+			}
+            waflash.statusElement.innerHTML = text;
+            waflash.showStatus();
+        },
+        showStatus(){
+            waflash.statusElement.style.display = 'block';
+        },
+        hideStatus(){ 
+            waflash.statusElement.style.display = 'none';
+        },
+        unload(){},
+    };	
+
+    window.onerror = function(message, url, line){
+        waflash.setStatus('An error occurred. Reload the Flash file, or Open the another Flash file.');
+    };
+
+    _getid('canvas').addEventListener("keydown", function(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+    });
+    _getid('canvas').addEventListener("mousedown", function(ev) {
+		//console.log(ev);
+		if(ev.button==0){
+			_getid('context-menu').style.display='none';
+		}
+    });
+	function contextmenu_event(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+		if(loaded){
+			var a=_getid('context-menu');
+			var b=_getid('canvas');
+			a.style.display='block';
+			var x=ev.pageX || ev.clientX;
+			var y=ev.pageY || ev.clientY;
+			if(x+a.offsetWidth>b.offsetWidth) x=b.offsetWidth-a.offsetWidth;
+			if(y+a.offsetHeight>b.offsetHeight) y=b.offsetHeight-a.offsetHeight;
+			a.style.left=x+'px';
+			a.style.top=y+'px';			
+		}
+    }
+    _getid('canvas').addEventListener("contextmenu", contextmenu_event);
+    _getid('context-menu').addEventListener("contextmenu", contextmenu_event);
+	_getid('menu1').addEventListener("contextmenu", contextmenu_event);
+
+    document.onmousedown = function(ev) {
+        var a=_getid('canvas');
+        if(ev.target == a){
+			a.focus();
+		}
+	};
+
+	createWaflashModule(waflash).then(function(o){
+		loaded=true;
+		_getid('loading').style.display='none';
+		_getid('menu1').onclick=function(){
+			proc_fullscreen();
+			_getid('context-menu').style.display='none';
+		}
+		loadswf(waflash);
+    });
+}
+
+function proc_resize(){
+	if(typeof(Event) === 'function'){
+		window.dispatchEvent(new Event('resize'));
+	}else{
+		var evt = window.document.createEvent('UIEvents'); 
+		evt.initUIEvent('resize', true, false, window, 0); 
+		window.dispatchEvent(evt);
+	}
+}
+function exitHandler(){
+	if(!loaded)return;
+	var a=_getid('canvas');
+	var b=_getid('menu1');
+	if(isfullscreen()){
+		b.innerHTML='Exit fullscreen (Emulator 2)';
+	}else{
+		b.innerHTML='Enter fullscreen (Emulator 2)';
+		a.style.width='100%';
+		a.style.height='100%';
+	}
+	setTimeout(function(){
+		proc_resize();
+	},100);
+	a.focus();
+}
+document.addEventListener('webkitfullscreenchange', exitHandler, false);
+document.addEventListener('mozfullscreenchange', exitHandler, false);
+document.addEventListener('fullscreenchange', exitHandler, false);
+document.addEventListener('MSFullscreenChange', exitHandler, false);
+
+function isfullscreen(){
+	var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
+		(document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+		(document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+		(document.msFullscreenElement && document.msFullscreenElement !== null);
+	return isInFullScreen;
+}
+function proc_fullscreen(){
+	if(!isfullscreen() && screen.width>0 && screen.height>0){
+		var a=_getid('canvas');
+		a.style.width = screen.width+'px';
+		a.style.height = screen.height+'px';
+		setTimeout(function(){
+			proc_resize();
+			setTimeout(function(){
+				proc_fullscreen2();
+			},100);
+		},100);
+	}else{
+		proc_fullscreen2();
+	}
+}
+function proc_fullscreen2(){
+	try{		
+		var elem=document.documentElement;			
+		if(!isfullscreen()){
+			if (elem.requestFullscreen) {
+				elem.requestFullscreen();
+			} else if (elem.mozRequestFullScreen) { 
+				elem.mozRequestFullScreen();
+			} else if (elem.webkitRequestFullscreen) {
+				elem.webkitRequestFullscreen();
+			} else if (elem.msRequestFullscreen) { 
+				elem.msRequestFullscreen();
+			}			
+		}else{
+			if(document.exitFullscreen) {
+				document.exitFullscreen();
+			} else if (document.webkitExitFullscreen) {
+				document.webkitExitFullscreen();
+			} else if (document.mozCancelFullScreen) {
+				document.mozCancelFullScreen();
+			} else if (document.msExitFullscreen) {
+				document.msExitFullscreen();
+			}
+		}
+	}catch(err){
+		alert(err+'');
+	}
+}
+
+function loadswf(waflash){
+	if(!(parent && parent.gbloburl))return;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', parent.gbloburl);
+	xhr.responseType = 'arraybuffer';
+    xhr.onload = function(){
+		if(this.status == 200){
+			var uint8Array = new Uint8Array(this.response);
+			var buf = waflash._malloc(uint8Array.length);
+			waflash.HEAP8.set(uint8Array, buf);
+			var res = waflash.ccall('reopenBuffer', 'int', ['string', 'number', 'number'], ['input.swf', buf, uint8Array.length]);
+			waflash._free(buf);
+			if(res == 0){
+				waflash.setStatus('Error. Failed to laod the Flash file.');
+            }
+		}else{
+			waflash.setStatus('Error. Can not get a file data.');
+		}
+	};
+	xhr.onerror = function(){
+		waflash.setStatus('Error. Can not get a file data.');
+	};
+	xhr.send();
+}
+init();
+</script>
+</body>
+</html>
---
+++ js/common2.js
@@ -0,0 +1,203 @@
+﻿function setCookie(name, value, expires) { 
+  if (!expires) expires=1000*60*60*24*365*5;
+  path="/";
+  domain=document.domain;
+  secure=false;
+  var today = new Date(); 
+  today.setTime( today.getTime() ); 
+  var expires_date = new Date( today.getTime() + (expires) ); 
+  document.cookie = name + "=" +escape( value ) + 
+          ( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) + //expires.toGMTString() 
+          ( ( path ) ? ";path=" + path : "" ) + 
+          ( ( domain ) ? ";domain=" + domain : "" ) + 
+          ( ( secure ) ? ";secure" : "" ); 
+  domain=".overbits.net";
+  document.cookie = name + "=" +escape( value ) + 
+          ( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) + //expires.toGMTString() 
+          ( ( path ) ? ";path=" + path : "" ) + 
+          ( ( domain ) ? ";domain=" + domain : "" ) + 
+          ( ( secure ) ? ";secure" : "" ); 
+} 
+
+function getCookie( name ) {
+  var nameOfCookie = name + "=";
+  var x = 0;
+  while ( x <= document.cookie.length ) {
+    var y = (x+nameOfCookie.length);
+    if ( document.cookie.substring( x, y ) == nameOfCookie ) {
+      if ( (endOfCookie=document.cookie.indexOf( ";", y )) == -1 )
+         endOfCookie = document.cookie.length;
+      return unescape( document.cookie.substring( y, endOfCookie ) );
+    }
+    x = document.cookie.indexOf( " ", x ) + 1;
+    if ( x == 0 ) break;
+  }
+  return "";
+}
+
+function _getid(id){
+	return document.getElementById(id);
+}
+
+function trim(str) {
+	if (!str || !str.replace) str='';
+  	return str.replace(/^\s*|\s*$/g,"");
+}
+
+function html_entity_encode(str){
+	if (!str || !str.replace) str='';
+  str = str.replace(/&/gi, "&amp;");
+  str = str.replace(/>/gi, "&gt;");
+  str = str.replace(/</gi, "&lt;");
+  str = str.replace(/\"/gi, "&quot;");
+  str = str.replace(/\'/gi, "&#039;");
+  return str;
+}
+
+function shortstring(s,len){
+	if (!s) s='';
+	if (s.length > len) s=s.substr(0,len)+"...";
+	return s;
+}
+
+function cutstringmiddle(s,len,left,right){
+	if (!s) s='';
+	if (s.length <= len) return s;			
+	var s1,s2;	
+	s1=s.substr(0,left);
+	s2=s.substr(s.length-right,s.length);	
+	return s1+'.....'+s2;
+}
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+  number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function (n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+
+function getWindowWidth(){
+    var windowWidth = 0;
+    if (typeof(window.innerWidth) == 'number'){
+        windowWidth = window.innerWidth;
+    }else{
+        var ieStrict = document.documentElement.clientWidth;
+        var ieQuirks = document.body.clientWidth; 
+        windowWidth = (ieStrict > 0) ? ieStrict : ieQuirks;
+    }
+	if(!windowWidth) windowWidth=0;
+    return windowWidth;
+}
+function getWindowHeight(){
+    var windowHeight = 0;
+    if (typeof(window.innerHeight) == 'number'){
+        windowHeight = window.innerHeight;
+    }else{
+        var ieStrict = document.documentElement.clientHeight; 
+        var ieQuirks = document.body.clientHeight;
+        windowHeight = (ieStrict > 0) ? ieStrict : ieQuirks;
+    }
+	if(!windowHeight) windowHeight=0;
+    return windowHeight;
+}
+
+function getScrollLeft(){
+    var scrollLeft;
+	if(document.body && document.body.scrollLeft){
+		scrollLeft = document.body.scrollLeft;
+	}else if(document.documentElement && document.documentElement.scrollLeft){
+		scrollLeft = document.documentElement.scrollLeft;
+	}
+	if(!scrollLeft) scrollLeft=0;
+    return scrollLeft;
+}
+
+function getScrollTop(){
+    var scrollTop;
+	if(document.body && document.body.scrollTop){
+		scrollTop = document.body.scrollTop;
+	}else if(document.documentElement && document.documentElement.scrollTop){
+		scrollTop = document.documentElement.scrollTop;
+	}
+	if(!scrollTop) scrollTop=0;
+    return scrollTop;
+} 
+
+var messagetimer=null;
+function show_message(s,x,y,padding,timeout){
+	if (!x) x=10;
+	if (!y) y=10;
+	if (!padding) padding=5;
+	if (!timeout) timeout=2000;
+
+	var kind=1;
+	for(var i=1; i <= 4; i++){
+		var s1="layer_message";
+		if (i>1) s1="layer_message"+i;
+		var obj=document.getElementById(s1);
+		if (obj){
+			kind=i;
+			break;
+		}
+	}
+			
+	obj.style.left="1px";
+	obj.style.top="1px";		
+	obj.innerHTML="<label>"+s+"</label>";
+	obj.style.display="";	
+	
+	if (kind==1) {
+		x=getScrollLeft()+x;	
+		y=getScrollTop()+y;
+	} else if (kind==2) {
+		x=getScrollLeft()+((getWindowWidth()-obj.clientWidth) / 2);
+		y=getScrollTop()+((getWindowHeight()-obj.clientHeight) / 2);
+	} else if (kind==3) {
+		x=document.body.offsetWidth-obj.clientWidth-5;
+		y=getScrollTop()+y;
+	} else {
+		x=getScrollLeft()+((getWindowWidth()-obj.clientWidth) / 2);
+		y=getScrollTop()+y;
+	}
+	x=parseInt(x);
+	y=parseInt(y);
+	
+	obj.style["border"]="1px solid #000000";
+	obj.style["padding"]=padding+"px";
+	obj.style.left=x+"px";
+	obj.style.top=y+"px";
+	
+	if (messagetimer) clearTimeout(messagetimer);
+	messagetimer=setTimeout(hide_message, timeout);
+}
+
+function hide_message(){
+	for(var i=1; i <= 4; i++){
+		var s1="layer_message";
+		if (i>1) s1="layer_message"+i;
+		var obj=document.getElementById(s1);
+		if (obj){
+			obj.style.display="none";
+		}
+	}
+}
+
+function _getfrmdoc(ifrm){
+	return (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
+}
